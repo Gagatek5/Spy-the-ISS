6a1ba092adff52baa46168b88207cd875bb497b7
Gagatek5 <tomek.twa@me.com>
2018-09-22 14:22:42 +0200

    Initial Commit

---
+++ Spy The ISS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spy The ISS
+//
+//  Created by Tom on 22/09/2018.
+//  Copyright © 2018 Tom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
